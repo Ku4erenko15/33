2e5ea7c4bb74292a042338d5c9a725ba93007b32
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-04-04 12:44:03 +0300

    Add files via upload

---
+++ 33/Kit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33
+{
+    class Kit:Item
+    {
+
+        public List<Product> Products { get; set; }
+
+        public Kit(string name, decimal price, List<Product> products)
+            : base(name, price) => Products = products;
+
+        public override void Print()
+        {
+            Console.WriteLine($"");
+        }
+
+    }
+}
